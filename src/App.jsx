58c4b63acc4aa360024c10bb1c2cec071086eda8
James <yigitkasapoglu95@gmail.com>
2023-03-14 04:12:06 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/pqf-khg1oLcKWzGZ/scene.splinecode" />
+  );
+}
